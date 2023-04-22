2d83f1b692b03fb1646539c7458dc5a7e75cb766
StephAT <110427909+StephAT@users.noreply.github.com>
2023-04-22 11:24:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-from padelpy import padeldescriptor
+#from padelpy import padeldescriptor
 import subprocess
 import os
 import base64
